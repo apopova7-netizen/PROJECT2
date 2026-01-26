36d8cf8cb29c6a896a9a1888bf0f85156ab05444
apopova7-netizen <a.popova7@g.nsu.ru>
2026-01-26 23:17:11 +0700

    Update Student2.h

--- include/Student2.h
+++ include/Student2.h
@@ -1,6 +1,3 @@
-//
-// Created by Lenovo on 22.01.2026.
-//
 
 #ifndef PROJECT2_STUDENT2_H
 #define PROJECT2_STUDENT2_H
@@ -16,29 +13,24 @@ typedef struct list {
 }LIST;
 
 void Swap(int* a1, int* a2);
-void Reverse(int arr[], int start, int end);
+void Reverse(int* arr, int start, int end);
 void Sorting(int* arr, int n);
+int NoDuplicate(int arr[],int n);
 void PrintPermutation(int* perm,int n);
-
 bool NextPermutationNarayana(int* arr, int n);
 bool PrevPermutationNarayana(int* arr, int n);
-
 void PermutationNarayana(int arr[], int n,
                           void (*callback)(int* perm, int n));
 void PermutationReverseNarayana(int arr[], int n,
                           void (*callback)(int* perm, int n));
-
 bool ChooseStepPermutationNarayana(int arr[], int n, int direction);
 void ChoosePermutationNarayana(int arr[], int n,void (*callback)
                                        (int* perm, int n), int direction);
-
-
-long Factorial(int n);
-void PermutationFromIndex(int arr[], int n,
-                        long index, int result[]);
+int Factorial(int n);
+void PermutationFromIndex(int* arr, int n,
+                         int index, int result[]);
 void PermutationsFactorialSystem(int arr[],
         int n,void (*callback)(int* perm, int n));
-
 LIST* CreateList();
 void Fill(const LIST* list,int* arr);
 void Insert(LIST* list, int value, int indInsert);
@@ -47,13 +39,13 @@ void Pop(LIST* list);
 void DeletingListElements(LIST* list);
 int NextInversionTable(int* inv, int n);
 void PermutationFromInversionTable(const int* invTable, int n,const int* arr,int* res);
-
 void Sorting2dBubble(int** arr, int quantityPerm, int n);
 void PermutationsInversionTable(int arr[],int n,void (*callback)(int* perm, int n));
-
 void PermutationsPlusOne(int arr[], int n,
                             void (*callback)(int* perm, int n));
+#define MAX_LENGTH_ARRAY 12
 
 #endif //PROJECT2_STUDENT2_H
+
 
 
